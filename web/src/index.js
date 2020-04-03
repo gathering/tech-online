8c2d97cc807199b45363fd52f283fd70b791d0c1
Jo Emil Holen <jo-emil@outlook.com>
2020-04-03 13:05:03 +0200

    set base name, and fix dependency directory

--- web/src/index.js
+++ web/src/index.js
@@ -5,7 +5,7 @@ import App from './components/App';
 import './styles/main.scss';
 
 render(
-    <Router>
+    <Router basename="/tech-online">
         <App />
     </Router>,
     document.getElementById('root')
