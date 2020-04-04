da37d58acdca5a2159e0db0c794e9b1aba4c7acc
Jo Emil Holen <jo-emil@outlook.com>
2020-04-04 22:20:31 +0200

    add polyfill for async/await

--- web/src/index.js
+++ web/src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 import { UserProvider } from './store/userContext';
+import 'regenerator/runtime';
 import './styles/main.scss';
 
 render(
