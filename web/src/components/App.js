814280639ea6be83c8e3a014528157814193b7f1
S1LV3R <s1lv3r@corax.team>
2021-03-26 13:27:19 +0100

    create links to admin page

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -22,14 +22,18 @@ const App = () => {
                     )}
                 </div>
                 <div className="nav-right">
-                    {loggedIn && <NavLink to="/logout">Log out</NavLink>}
                     {!loggedIn && (
                         <NavLink to="/login" className="participate-link">
                             Login
                         </NavLink>
                     )}
-                    {/* <NavLink to="/demo">Demo</NavLink> */}
+                    {user.admin && (
+                        <NavLink to="/admin" className="participate-link">
+                            Admin
+                        </NavLink>
+                    )}
                     <NavLink to="/status">Status</NavLink>
+                    {loggedIn && <NavLink to="/logout">Log out</NavLink>}
                 </div>
             </header>
             <main>
@@ -40,6 +44,7 @@ const App = () => {
                     <Route path="/participate" component={Views.Participate} />
                     <Route path="/status/:id?" component={Views.Status} />
                     <Route path="/logout" component={Views.Logout} />
+                    <Route path="/admin" component={Views.Admin} />
                     {/* <Route path="/demo" component={Views.Demo} /> */}
                 </Switch>
             </main>
