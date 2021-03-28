b5ce23534266b1cd0190ac5db004201bb52c23e0
S1LV3R <s1lv3r@corax.team>
2021-03-28 13:18:50 +0200

    formatting and commenting

--- client/web/scripts/index.js
+++ client/web/scripts/index.js
@@ -1,5 +1,3 @@
-
-
 const kropp = document.getElementById("kropp");
 const dato = document.getElementById("dato");
 const imageMagic = createImageMagic({
@@ -7,42 +5,62 @@ const imageMagic = createImageMagic({
   images,
 });
 
-function jadda() {
-  kropp.classList.add("success");
-  imageMagic.show();
-}
-function neida() {
-  kropp.classList.remove("success");
-  imageMagic.hide();
-}
-
-function sjekk() {
-  fetch("https://techo.gathering.org/ping", {
-    cache: "no-cache",
-    mode: "cors",
-  })
-    .then((response) => {
-      if (response.ok) {
-        jadda();
-      } else {
-        neida();
-      }
-    })
-    .catch((err) => {
-      neida();
-      console.warn(err);
-    });
-}
+/**
+ * padding number with leading zeroes so it becomes two digits no matter what
+ *
+ * @param {number|string} thing the number to pad
+ * @return {string} the padded number
+ */
 function pad(thing) {
   return thing.toString().padStart(2, 0);
 }
 
+/**
+ * sets the date on the website
+ *
+ */
 function setDate() {
   const date = new Date();
 
   dato.innerText = `${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(
     date.getSeconds()
   )}`;
+}
+
+/**
+ * does stuff to the website when stuff works
+ *
+ */
+function jadda() {
+  kropp.classList.add("success");
+  imageMagic.show();
+}
+
+/**
+ * does stuff to the website when stuff doesnt work
+ *
+ */
+function neida() {
+  kropp.classList.remove("success");
+  imageMagic.hide();
+}
+
+/**
+ * checks the connection to make sure the internet works
+ *
+ */
+function sjekk() {
+  fetch("https://techo.gathering.org/ping", {
+    cache: "no-cache",
+    mode: "cors",
+  })
+    .then((response) => {
+      response.ok ? jadda() : neida();
+    })
+    .catch((err) => {
+      neida();
+      console.warn(err);
+    });
 }
 
 sjekk();
