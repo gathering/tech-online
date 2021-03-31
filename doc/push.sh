69362427c377cdb83a9d12c387561b7479c246bf
Kristian Lyngstol <kly@kly.no>
2021-03-31 13:10:56 +0200

    Add push-script for documentation

---
+++ doc/push.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+set -e
+mkdir -p build
+
+ID=$(GET https://techo.gathering.org/api/documents/?family=ref-net | jq .[].id | sed 's/"//g')
+
+echo -n $ID > build/ref-net-id
+jq --rawfile foo TechOnline.rst --rawfile id build/ref-net-id '. +{content:$foo} +{id:$id}' < net-ref.json > build/net-ref.json.out
+lwp-request -m PUT -C "$(cat ~/techopw)"  https://techo.gathering.org/api/document/${ID} < build/net-ref.json.out
+
