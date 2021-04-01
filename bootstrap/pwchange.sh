abe487d712f87bc8e5ae2c4377990fdfbb32c1eb
Kristian Lyngstol <kly@kly.no>
2021-04-01 17:04:20 +0200

    pwchange-fix

--- bootstrap/pwchange.sh
+++ bootstrap/pwchange.sh
@@ -2,8 +2,8 @@
 
 station=$2
 pw=$2
-GET -C "$(cat ~/techopw)" "https://techo.gathering.org/api/admin/stations/?track=net&shortname=$1" | jq '.[]' > foo.json
+GET -C "$(cat /techopw)" "https://techo.gathering.org/api/admin/stations/?track=net&shortname=$1" | jq '.[]' > foo.json
 ID=$(cat foo.json | jq .id | sed 's/"//g')
 echo $ID
-cat foo.json | jq ". +{credentials: \"$2\"}" | lwp-request -m PUT -C "$(cat ~/techopw)" "https://techo.gathering.org/api/station/${ID}"
+cat foo.json | jq ". +{credentials: \"$2\"}" | lwp-request -m PUT -C "$(cat /techopw)" "https://techo.gathering.org/api/station/${ID}"
 
