a19c6806c4b7b78f9ea8b53bea21d3cbe06a8c26
S1LV3R <s1lv3r@corax.team>
2022-04-14 21:36:04 +0200

    login post

--- web/src/views/Logout.tsx
+++ web/src/views/Logout.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import { httpPost } from '../common/api';
 import { actions, useUserDispatch } from '../store/userContext';
 
 function Logout() {
+  httpPost('oauth/logout');
+
   const dispatch = useUserDispatch();
   dispatch({ type: actions.LOGOUT });
 
